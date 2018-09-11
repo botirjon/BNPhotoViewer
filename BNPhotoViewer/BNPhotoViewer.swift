a0ab72b87570b6f70f7962ebc449b7b265af0b44
botirjon <botirjon.nasridinov@gmail.com>
2018-09-11 17:55:43 +0500

    Initial Commit

---
+++ BNPhotoViewer/BNPhotoViewer.swift
@@ -0,0 +1,16 @@
+//
+//  BNPhotoViewer.swift
+//  BNPhotoViewer
+//
+//  Created by botirjon nasridinov on 11/09/2018.
+//  Copyright © 2018 Orient Software Team. All rights reserved.
+//
+
+import UIKit
+
+public class BNPhotoViewer: UIViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
