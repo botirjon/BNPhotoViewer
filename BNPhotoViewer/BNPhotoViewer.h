57182b805684e370396e08d692760d335cd19ba3
botirjon <botirjon.nasridinov@gmail.com>
2018-09-11 17:19:38 +0500

    Initial Commit

---
+++ BNPhotoViewer/BNPhotoViewer.h
@@ -0,0 +1,19 @@
+//
+//  BNPhotoViewer.h
+//  BNPhotoViewer
+//
+//  Created by botirjon nasridinov on 11/09/2018.
+//  Copyright © 2018 Orient Software Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BNPhotoViewer.
+FOUNDATION_EXPORT double BNPhotoViewerVersionNumber;
+
+//! Project version string for BNPhotoViewer.
+FOUNDATION_EXPORT const unsigned char BNPhotoViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BNPhotoViewer/PublicHeader.h>
+
+
